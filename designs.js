2d5d415ed940ae0b52e9b68fd2fb83adce8a2de6
FranklynThompson <frankistone28@outlook.com>
2018-06-26 13:44:31 +0000

    added the reset button

--- designs.js
+++ designs.js
@@ -9,6 +9,9 @@ const table = $("#pixelCanvas");
 const body = $("body");
 // const numInput = $("[type='number']");
 
+// Select reset button
+const resetBtn = $("#reset");
+
 // When size is submitted by the user, call makeGrid()
 const form = $("#sizePicker");
 
@@ -56,6 +59,11 @@ table.on("click", "td", function() {
         $(this).css({ backgroundColor: "rgb(255, 255, 255)" });
     }
 
+});
+
+// Reset the Grid
+resetBtn.on("click", function() {
+    table.empty();
 });
 
 // change the the color property of every child element within the body to 
